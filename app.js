e938f35b380c365f3475128bdf4a1d2ec510e65d
Taylorm1011 <111470063+Taylorm1011@users.noreply.github.com>
2023-09-15 13:46:48 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const https = require('https');
+
+function fetDef(term) {
+    try {
+        const request = https.get{
+            'https://dictionaryapi.com/api/v3/references/collegiate/json/test?key=152ee421-77f2-4bb7-bd3c-6f1027083273'
+            (response) => {
+                let body = "";
+                response.on("data", (data) => {
+                    body += data.toString();
+                });
+                response.on('end', () => {
+                    const definition = JSON.parse(body);
+                    console.log(definition[0].shortdef);
+                })
+            }
+
+        }
+        request.on('error', (error) => console.error(error.message));
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+const query = process.argv.slice(2);
+query.forEach(getDef);
